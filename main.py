4cc44c091aabc330b4b40bd2b2fc5d6dbb04fef9
Aaron Jevon Benedict Kongdoh <abenedict01@student.ciputra.ac.id>
2024-11-17 10:59:51 +0800

    main and function

---
+++ main.py
@@ -0,0 +1,27 @@
+from function import *
+
+# Initializaiton
+S = Himpunan([1,2,3,4,5,6,7,8,9])
+h1 = Himpunan([1, 2, 3])
+h2 = Himpunan([3, 4, 5])
+
+print(len(h1))  # Output: 3
+print(3 in h1)  # Output: True
+print(h1 == h2)  # Output: False
+
+h1 += 4  # Menambah elemen 4 ke h1
+print(h1)  # Output: {1, 2, 3, 4}
+
+h3 = h1 / h2  # Irisan
+print(h3)  # Output: {3, 4}
+
+h4 = h1 + h2  # Gabungan
+print(h4)  # Output: {1, 2, 3, 4, 5}
+
+h5 = h1 - h2  # Selisih
+print(h5)  # Output: {1, 2]}
+
+h6 = h1.complement(S)  # Komplemen
+print(h6)  # Output: {4,5,6,7,8,9}
+
+print(abs(h1))  # 8
